11619158dd01957a0a8d3a150fee5ca93a86b79e
Michael Safyan <michaelsafyan@gmail.com>
2016-05-19 18:25:29 -0400

    Improve support for virtualenv.

--- my-nix-setup/etc/bashrc.d/prompt.sh
+++ my-nix-setup/etc/bashrc.d/prompt.sh
@@ -5,16 +5,12 @@ if [[ "$C9_USER" != "" ]] ; then
   USER_AT_HOST='${C9_USER}@${C9_PROJECT}'
 fi
 
-GIT_COMPLETION=''
-if [ $(type __git_ps1 >/dev/null 2>&1 ; echo $?) -eq 0 ] ; then
-  GIT_COMPLETION='$(__git_ps1 " (%s)")'
-fi
-
+CONTEXT_COMPLETION='$(prompt_context)'
 XTERM_RED='01;31'
 XTERM_GREEN='01;32'
 BASIC_COLOR=$XTERM_GREEN
 DIRECTORY_COLOR='01;34'
-GIT_STATUS_COLOR='0;35'
+CONTEXT_STATUS_COLOR='0;35'
 PROMPT_SYMBOL='\$'
 if [ $EUID -eq 0 ] ; then
   PROMPT_SYMBOL='#'
@@ -30,11 +26,11 @@ if [[ "$USER" == "root" ]] ; then
 fi
 
 function set_uncolored_prompt() {
-  export PS1="[${USER_AT_HOST} \w${GIT_COMPLETION}]${PROMPT_SYMBOL} "
+  export PS1="[${USER_AT_HOST} \w${CONTEXT_COMPLETION}]${PROMPT_SYMBOL} "
 }
 
 function set_colored_prompt() {
-  export PS1="\[\033[${BASIC_COLOR}m\][${USER_AT_HOST} \[\033[${DIRECTORY_COLOR}m\]\w\[\033[00m\]\[\033[${GIT_STATUS_COLOR}m\]${GIT_COMPLETION}\[\033[00m\]\[\033[${BASIC_COLOR}m\]]${PROMPT_SYMBOL}\[\033[00m\] "
+  export PS1="\[\033[${BASIC_COLOR}m\][${USER_AT_HOST} \[\033[${DIRECTORY_COLOR}m\]\w\[\033[00m\]\[\033[${CONTEXT_STATUS_COLOR}m\]${CONTEXT_COMPLETION}\[\033[00m\]\[\033[${BASIC_COLOR}m\]]${PROMPT_SYMBOL}\[\033[00m\] "
 }
 
 function set_prompt() {
@@ -44,4 +40,5 @@ function set_prompt() {
   esac
 }
 
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 set_prompt
